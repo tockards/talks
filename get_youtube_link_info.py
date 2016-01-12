2006ce84fc9214bdc31c0ccdd2e7151400d79292
tockards <tockards@ska.ac.za>
2016-01-13 00:11:21 +0200

    completed script and added new talk

--- get_youtube_link_info.py
+++ get_youtube_link_info.py
@@ -1,9 +1,18 @@
 import urllib2
-from HTMLParser import HTMLParser
+import re
+import sys
 
-# create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
 
+'''Avoided using beautiful soup and requests and api, I wanted to stick to 
+   standard lib as far as possible
+'''
+
+TITLE_RE = '<title.*?>(.+?)</title>'
+PUBLISHED_RE = '<strong class="watch-time-text">(.+?)</strong>'
+
+
+pattern = re.compile(TITLE_RE)
+pattern2 = re.compile(PUBLISHED_RE)
 
 def get_page(youtube_link):
 
@@ -13,7 +22,21 @@ def get_page(youtube_link):
         print (exc)
 
     html = link.read()
+    titles=re.findall(pattern,html) 
+    publish_date=re.findall(pattern2,html) 
+   
+    try: 
+        t = titles[0]
+        pd = publish_date[0]
+    except Exception as exc:
+        print (exc)
+    
+    return titles[0], publish_date[0]
 
-    return html
+if __name__ == "__main__":
+    if sys.argv[2] == 'md':
+        t, pd = get_page(sys.argv[1])
+        print ("[{}]({})".format(t, sys.argv[1]))
+        print ("{}".format(pd))
 
 
